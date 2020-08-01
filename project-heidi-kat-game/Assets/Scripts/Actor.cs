5cdb2570f681795c1dd9b2f65c090dabfdabbb9b
jermzlar <jermzlar@yahoo.com>
2020-07-31 23:42:23 -0700

    Added part for taking input from Ink
    Added in a skeleton for taking input from Ink. TODO I get an error when using GetComponent on the Actor GameObject.

--- project-heidi-kat-game/Assets/Scripts/Actor.cs
+++ project-heidi-kat-game/Assets/Scripts/Actor.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class Actor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+	public Sprite[] emotionSprites; //https://www.youtube.com/watch?v=v5sU1Aidzwo - 15 min
+	SpriteRenderer spRend;
+	public int ID; //Left = 0, Right = 1
+	public enum CharacterStats
     {
-        
+    	normal,attack
+    }
+
+    void Awake()
+    {
+        //myStat = CharacterStats.normal; //27:09
+        spRend = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeStat(string statName)
+    {
+    	StartCoroutine(statName + "Stat");
+    }
+
+    IEnumerator defaultStat()
+    {
+    	//placeholder
+    	yield return null;
+    }
+
+    IEnumerator attackStat()
+    {
+    	//placeholder
+    	yield return null;    	
     }
 
     // Update is called once per frame
--- project-heidi-kat-game/Assets/Scripts/CharacterManager.cs
+++ project-heidi-kat-game/Assets/Scripts/CharacterManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +7,60 @@ public class CharacterManager : MonoBehaviour
 {
 
 	public GameObject[] characters;
-    // Start is called before the first frame update
+	public List<GameObject> actorsList = new List<GameObject>();
+	[SerializeField]
+	Vector3 leftActorPosition, rightActorPosition;
+
+	List<Actor> activeActors = new List<Actor>();
+
     void Start()
+    {/*
+        for(int i = 0; i < characters.Length; i++)
+        {
+        	GameObject newActor = Instantiate(characters[i]);
+        	newActor.SetActive(false);
+        	newActor.name = characters[i].name;
+        	actorsList.Add(newActor);
+        }*/
+    }
+/*
+    public void PlaceActors(string leftActorName, string rightActorName)
     {
-        
+    	foreach (GameObject gO in actorsList)
+    	{
+    		if(gO.name == leftActorName)
+    		{
+    			gO.SetActive(true);
+    			gO.getComponent<Actor>().ID = 0;
+    			activeActors.Add(gO.getComponent<Actor>());
+    			gO.transform.position = leftActorPosition;
+    		} else if(gO.name == rightActorName)
+    		{
+    			gO.SetActive(true);
+    			gO.getComponent<Actor>().ID = 1;
+    			activeActors.Add(gO.getComponent<Actor>());
+    			gO.transform.position = rightActorPosition;
+    		}
+
+    	}
     }
 
-    // Update is called once per frame
+    //ID Left = 0, right = 1
+    public void ChangeActorStat(string stat, int ID)
+    {
+    	foreach (Actor actor in activeActors)
+    	{
+    		if (actor.gameObject.activeInHierarchy)
+    		{
+    			actor.ChangeStat(stat);
+    		}
+    	}
+
+    }
+*/
     void Update()
     {
         
     }
+
 }
