e82419dad7d5627831ae688113e2c1333a27b9cb
corruptusx1 <32150340+corruptusx1@users.noreply.github.com>
2020-08-02 13:59:43 -0700

    Added Hide/Show UI

---
+++ project-heidi-kat-game/Assets/Scripts/HideShowUI.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideShowUI : MonoBehaviour
+{
+    [SerializeField]
+    public CanvasGroup dialogueChoices = null;
+
+    [SerializeField]
+    public CanvasGroup statusBox = null;
+
+    [SerializeField]
+    public CanvasGroup dialogueBox = null;
+
+    [SerializeField]
+    public Sprite showUI = null;
+
+    [SerializeField]
+    public Sprite hideUI = null;
+
+    private Image buttonImage;
+
+    public void Toggle()
+    {
+        if (dialogueBox.alpha == 1)
+        {
+            ToggleCanvasGroups(false);
+            ToggleButtonImage(false);
+        }
+        else
+        {
+            ToggleCanvasGroups(true);
+            ToggleButtonImage(true);
+        }
+
+
+    }
+
+    public void ToggleCanvasGroups(bool toggle)
+    {
+        if (toggle)
+        {
+            dialogueChoices.alpha = 1;
+            dialogueChoices.blocksRaycasts = true;
+            dialogueChoices.interactable = true;
+
+            statusBox.alpha = 1;
+            statusBox.blocksRaycasts = true;
+            statusBox.interactable = true;
+
+            dialogueBox.alpha = 1;
+            dialogueBox.blocksRaycasts = true;
+            dialogueBox.interactable = true;
+        }
+        else
+        {
+            dialogueChoices.alpha = 0;
+            dialogueChoices.blocksRaycasts = false;
+            dialogueChoices.interactable = false;
+
+            statusBox.alpha = 0;
+            statusBox.blocksRaycasts = false;
+            statusBox.interactable = false;
+
+            dialogueBox.alpha = 0;
+            dialogueBox.blocksRaycasts = false;
+            dialogueBox.interactable = false;
+        }
+    }
+
+    public void ToggleButtonImage(bool toggle)
+    {
+        if (toggle)
+            buttonImage.sprite = showUI;
+        else
+            buttonImage.sprite = hideUI;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonImage = transform.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
