18bee6a352fb194ce7cc06621b198e87e98e4046
corruptusx1 <32150340+corruptusx1@users.noreply.github.com>
2020-08-02 16:43:30 -0700

    Added Sounds to Sound Manager

--- project-heidi-kat-game/Assets/Scripts/SoundManager.cs
+++ project-heidi-kat-game/Assets/Scripts/SoundManager.cs
@@ -12,6 +12,45 @@ public class SoundManager : MonoBehaviour
     public AudioClip mouseClickSound = null;
 
     [SerializeField]
+    public AudioClip snatcherTheme2 = null;
+
+    [SerializeField]
+    public AudioClip boopbeepboop1 = null;
+
+    [SerializeField]
+    public AudioClip clatterandbang = null;
+
+    [SerializeField]
+    public AudioClip voltetscreech2 = null;
+
+    [SerializeField]
+    public AudioClip voltetscreech3 = null;
+
+    [SerializeField]
+    public AudioClip crash = null;
+
+    [SerializeField]
+    public AudioClip glassbreak = null;
+
+    [SerializeField]
+    public AudioClip pewpew = null;
+
+    [SerializeField]
+    public AudioClip chestburst = null;
+
+    [SerializeField]
+    public AudioClip woozy = null;
+
+    [SerializeField]
+    public AudioClip metalfootsteps = null;
+
+    [SerializeField]
+    public AudioClip rummage = null;
+
+    [SerializeField]
+    public AudioClip dragging = null;
+
+    [SerializeField]
     static AudioSource audioSrc;
     void Start()
     {
@@ -31,15 +70,49 @@ public class SoundManager : MonoBehaviour
 
     	switch (clip)
     	{
-    		case "body_snatcher_sound":
+    		case "body snatcher sound":
                 audioSrc.clip = snatcherSound;
 	    		break;
 	    	case "mouse_click":
 	    		audioSrc.clip = mouseClickSound;
 	    		break;
-            
-
-    	}
+            case "body snatcher theme 2":
+                audioSrc.clip = snatcherTheme2;
+                break;
+            case "boopbeepboop 1":
+                audioSrc.clip = boopbeepboop1;
+                break;
+            case "clatter and bang":
+                audioSrc.clip = clatterandbang;
+                break;
+            case "voltet screech 2":
+                audioSrc.clip = voltetscreech2;
+                break;
+            case "voltet screech 3":
+                audioSrc.clip = voltetscreech3;
+                break;
+            case "crash":
+                audioSrc.clip = crash;
+                break;
+            case "glass break":
+                audioSrc.clip = glassbreak;
+                break;
+            case "chestburst":
+                audioSrc.clip = chestburst;
+                break;
+            case "woozy":
+                audioSrc.clip = woozy;
+                break;
+            case "metal footsteps":
+                audioSrc.clip = metalfootsteps;
+                break;
+            case "rummage":
+                audioSrc.clip = rummage;
+                break;
+            case "dragging":
+                audioSrc.clip = dragging;
+                break;
+        }
 
         audioSrc.Play();
 
