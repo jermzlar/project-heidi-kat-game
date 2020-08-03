1d93c3db34ffa7a13359ee31ebd1c61d38dd6c6b
corruptusx1 <32150340+corruptusx1@users.noreply.github.com>
2020-08-02 17:28:50 -0700

    Added Music Manager

---
+++ project-heidi-kat-game/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField]
+    public AudioSource audioSrc;
+
+    [SerializeField]
+    public AudioClip opener = null;
+
+    [SerializeField]
+    public AudioClip ambientSpaceship = null;
+
+    [SerializeField]
+    public AudioClip ambienttunnels = null;
+
+    [SerializeField]
+    public AudioClip creepy = null;
+
+    [SerializeField]
+    public AudioClip combat1 = null;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void HandleMusic(string music)
+    {
+        switch(music)
+        {
+            case "opener":
+                audioSrc.clip = opener;
+                break;
+            case "ambient spaceship 1":
+                audioSrc.clip = ambientSpaceship;
+                break;
+            case "ambient tunnels 1":
+                audioSrc.clip = ambienttunnels;
+                break;
+            case "creepy":
+                audioSrc.clip = creepy;
+                break;
+            case "combat1":
+                audioSrc.clip = combat1;
+                break;
+            case "music fade":
+                audioSrc.Stop();
+                break;
+            default:
+                Debug.Log("Unknown Music Command: " + music);
+                break;
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
